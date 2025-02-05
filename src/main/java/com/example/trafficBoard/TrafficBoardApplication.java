6cbef1566ba5380c8de4ae47fef33a24ec3eb50a
bonyoung.bae <bp6115@naver.com>
2025-02-05 23:28:27 +0900

    # 초기 설정
    # 초기 설정

---
+++ src/main/java/com/example/trafficBoard/TrafficBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.trafficBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trafficBoard/TrafficBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trafficBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
